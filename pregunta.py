e8a9ce39e722dac18599d944d5696d61dc4e46d0
juan-quirozt <92911469+juan-quirozt@users.noreply.github.com>
2023-06-26 17:12:09 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -15,17 +15,20 @@ def clean_data():
 
     df.dropna(axis = 0, inplace = True)
     df.drop_duplicates(inplace = True)
-    df = df.drop('Unnamed: 0', axis = 1)
+    #df = df.drop('Unnamed: 0', axis = 1)
 
-    df[["sexo", "tipo_de_emprendimiento","idea_negocio","barrio","línea_credito"]] = df[["sexo", "tipo_de_emprendimiento","idea_negocio","barrio","línea_credito"]].apply(lambda x: x.astype(str).str.lower())
-    df = df.replace(to_replace = "(_)|(-)", value = " ", regex = True)    
-    df = df.replace(to_replace = "[,$]|(\.00$)", value = "", regex = True)
-
-    df.monto_del_credito = df.monto_del_credito.astype(int)
-    df.comuna_ciudadano = df.comuna_ciudadano.astype(float)
-
-    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, infer_datetime_format = True, errors = 'ignore', dayfirst = True)
-    df.fecha_de_beneficio = df.fecha_de_beneficio.dt.strftime("%Y/%m/%d")
+    df = df.applymap(lambda x: re.sub(r'[-_]'," ",str(x).lower()))
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace("\.00", "").replace({ r"[\D]" : '' },regex= True).astype(int)
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(float)
+    
+    def dateparse(date):
+        kek = date.split("/")
+        if len(kek[0]) == 4:
+            return datetime(int(kek[0]),int(kek[1]),int(kek[2]))
+        else:
+            return datetime(int(kek[2]),int(kek[1]),int(kek[0]))
+    
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(dateparse)
+    
     df.drop_duplicates(inplace = True)
-
     return df
